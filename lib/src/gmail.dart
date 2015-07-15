056875d9d40c8c0a63e0bbb97975a68a633416db
Kevin Moore <kevmoo@google.com>
2015-07-14 20:18:56 -0700

    use logging instead of print in gmail.dart

--- lib/src/gmail.dart
+++ lib/src/gmail.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:googleapis/gmail/v1.dart';
+import 'package:logging/logging.dart';
 
 import 'email_sender_credentials.dart';
 import 'server_utils.dart';
@@ -33,9 +34,6 @@ Future<Message> sendEmail(String subject, String body,
 
 $body''');
 
-  print('content:');
-  print(emailContent);
-
   var request = new Message()
     ..rawAsBytes = UTF8.encode(emailContent.toString());
 
@@ -46,13 +44,13 @@ $body''');
     sentMessage = await api.users.messages.send(request, senderEmailAccount);
   });
 
-  print([
+  Logger.root.info([
     sentMessage.historyId,
     sentMessage.id,
     sentMessage.labelIds,
     sentMessage.payload,
     sentMessage.snippet
-  ]);
+  ].toString());
 
   return sentMessage;
 }
